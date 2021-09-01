0cf138ccb85d8acc4a34a43a9283e50d883666f5
Trevor Foresta <trevforesta@gmail.com>
2021-09-01 00:09:51 -0400

    Added Backdrops, Specials, & More
    Added hats, background colors, custom "special" tots

--- backdrop.py
+++ backdrop.py
@@ -6,14 +6,13 @@
 
 import glob
 from PIL import Image, ImageOps
+import random 
 
-#Set pathway and iterator for generated tots
+#Set pathways and iterator for generated tots
 tots = glob.glob ('Generated/*')
 tots_iter = iter(tots)
+backdrops = glob.glob ('Backdrops/*')
 COUNT = 0
-
-#Blank template of larger size to use as backdrop
-background_img = Image.open("BGTemplate.png")
 
 # increment - counter used for naming convention
 def increment():
@@ -23,9 +22,10 @@ def increment():
 # attach_bg - pastes generated tots onto background
 def attach_bg():
     for tot in range(len(tots)):
+        background_img = Image.open(random.choice(backdrops))
         tot = Image.open(next(tots_iter))
         name = tot.filename
-        background_img.paste(tot, (150,200), mask=0)
+        background_img.paste(tot, (150,200), mask=tot)
         increment()
         background_img.save("GeneratedBG/" + (str(COUNT).zfill(4))+ ".png", "PNG")
 
